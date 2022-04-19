6aa440f453566e6a8c1d7b7916c05f47606eef36
Hugo Barral <hugo.barral@outlook.fr>
2022-04-19 22:27:13 +0200

    nouvelles instructions et commentaires

--- lib/asmtab.h
+++ lib/asmtab.h
@@ -3,23 +3,26 @@
 #include <stdio.h>
 
 enum op {
-   ADD = 1,
-   MUL,
-   SOU,
-   DIV,
-   COP,
-   AFC,
-   JMP,
-   JMF,
-   INF,
-   SUP,
-   EQU,
-   PRI,
-   AND,
-   OR,
-   NOT,
-   NOP,
-   CND
+   ADD = 1, // Addition des valeurs de deux adresses vers une autre
+   MUL, // Multiplication des valeurs de deux adresses vers une autre
+   SOU, // Soustraction des valeurs de deux adresses vers une autre
+   DIV, // Division deq valeurs de deux adresses vers une autre
+   COP, // Copie la valeur d'une adresse vers une autre adresse
+   AFC, // Affecte une valeur à une adresse
+   JMP, // Saut inconditionnel vers une instruction
+   JMF, // Saut vers une instruction si condition dans adresse est fausse
+   INF, // Comparaison inférieur des valeurs de deux adresses vers une autre
+   SUP, // Comparaison supérieur des valeurs de deux adresses vers une autre
+   EQU, // Comparaison égale de deux valeurs de deux adresses vers une adresse
+   PRI, // Affichage de la valeur d'une adresse
+   AND, // ET logique entre deux valeurs vers une adresse
+   OR, // OU logique entre deux valeurs vers une adresse
+   NOT, // NON logique d'une valeurs vers une adresse
+   CLL, // Sauvegarde de l'adresse de retour et décalage du base pointer lors d'un appel de fonction
+   PSH, // Pousse l'argument d'une fonction
+   RET, // Décalage du base pointer et sortie vers l'adresse de retour de la fonction
+   NOP, // Instruction vide
+   CND // Instruction vide précédent des conditions
 };
 
 typedef struct inst {
